2a09bc666adfb667fb78cf2a7f4f49818b1e54a3
yisakisaac <115116705+yisakisaac@users.noreply.github.com>
2022-10-05 14:33:10 -0700

    Add files via upload

---
+++ multiple regression lab 15.R
@@ -0,0 +1,18 @@
+library(poliscidata)
+
+#Question 1 determing dependent variable
+mean(states$ba_or_more)
+
+#Question 2 describing the dependent variable
+describe(states$ba_or_more)
+freq(states$ba_or_more)
+
+#Question 5 multiple regression model
+summary(lm(states$ba_or_more~states$unemploy+states$prcapinc))
+
+#Question 6 multiple regression graphic
+plot(x=states$ba_or_more,y=states$unemploy)
+plot(x=states$ba_or_more,y=states$unemploy,xlab="percent of population with a college degree or higher",ylab="unemployment rate",main="effect of unemployment rate on having a college degree")
+abline(lm(unemploy~ba_or_more+scale(prcapinc),data=states))
+symbols(x=states$ba_or_more,y=states$unemploy,add=T,inches=.25,fg="black",bg=416,circles=states$prcapinc)
+text(states$ba_or_more,states$unemploy,states$prcapinc,cex=.6,offset=-1,pos=2)
